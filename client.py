53b45086720dc1f76a58b6b86e27ca61a46649e6
Fengax <857514.leofeng@gmail.com>
2022-02-06 18:35:10 +1100

    update

--- client.py
+++ client.py
@@ -15,26 +15,20 @@ sock.close()
 
 print('ready to exchange messages\n')
 
-# listen for
-# equiv: nc -u -l 50001
-def listen():
+if listen == "y":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('0.0.0.0', sport))
 
     while True:
         data = sock.recv(1024)
         print('\rpeer: {}\n> '.format(data.decode()), end='')
-
-
-if listen == 'y':
-    listener = threading.Thread(target=listen, daemon=True);
-    listener.start()
 else:
+
     # send messages
     # equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('0.0.0.0', dport))
-    
+
     while True:
         msg = input('> ')
         sock.sendto(msg.encode(), (ip, sport))
